5eeb86222500dba703e5c5046f5601ccd7763df2
Nur sorfina <sorfinafaisal@gmail.com>
2024-09-03 23:50:42 +0800

    Updated HTML

---
+++ create_account.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/')
+def home():
+
+    return send_from_directory(BASE_DIR, 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
